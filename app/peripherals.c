5ac9bc1f2d6f2e475ed6f7289bf2d432e2c7a7a5
CRImier <crimier@yandex.ru>
2025-05-22 10:44:48 +0200

    peripherals.c: prototype EXTCOMIN driver (untested for now)

--- app/peripherals.c
+++ app/peripherals.c
@@ -5,6 +5,11 @@
 #include <hardware/pwm.h>
 #include <pico/stdlib.h>
 
+alarm_id_t g_extcomin_alarm = -1;
+uint32_t extcomin_alarm_ms = 500;
+
+static int64_t extcomin_alarm_callback(alarm_id_t _, void* __);
+
 void peripherals_init(void)
 {
     // charging pins
@@ -18,6 +23,10 @@ void peripherals_init(void)
 	mcp23017_gpio_put(PIN_FUSB_MUX_SEL, 1); // setting SEL to output before boot means the I2C bus can fuckin crash cuz of I2CZ-I2CR short-circuit while I2CZ is non-powered
 	mcp23017_gpio_set_dir(PIN_FUSB_MUX_SEL, GPIO_OUT);
     #endif
+    mcp23017_gpio_set_dir(PIN_DISP_EXTIN, GPIO_OUT);
+    //mcp23017_gpio_put(PIN_DISP_EXTIN, 0);
+    g_extcomin_alarm = 1;
+    (void)extcomin_alarm_callback(0, NULL);
 }
 
 #ifdef PIN_CHG_DIS
@@ -72,3 +81,21 @@ void fusbmux_low()
 }
 
 #endif
+
+// EXTCOMIN driver
+
+static int64_t extcomin_alarm_callback(alarm_id_t _, void* __)
+{
+    static bool extcomin_high = false;
+
+    // Extcomin output disabled?
+    if (g_extcomin_alarm < 0) {
+        return 0;
+    }
+
+	mcp23017_gpio_put(PIN_DISP_EXTIN, extcomin_high);
+    extcomin_high = !extcomin_high;
+
+    g_extcomin_alarm = add_alarm_in_ms(extcomin_alarm_ms, extcomin_alarm_callback, NULL, true);
+    return 0;
+}
