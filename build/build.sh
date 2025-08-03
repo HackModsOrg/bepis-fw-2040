5a4c7a2cf26711b83918eb2df31e806ff7baebaf
CRImier <crimier@yandex.ru>
2025-08-03 16:51:50 +0200

    build.sh now tries to build for blepis v2

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eux
-PICO_SDK_PATH=../3rdparty/pico-sdk cmake -DPICO_BOARD=bepis -DCMAKE_BUILD_TYPE=Debug ..; make
+PICO_SDK_PATH=../3rdparty/pico-sdk cmake -DPICO_BOARD=blepisv2 -DCMAKE_BUILD_TYPE=Debug ..; make
